78a859b209c766db6c8c3d209d596e0fcb5adc1e
VoynovDev <vladimir.voynov2001@mail.ru>
2024-06-24 00:38:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Menu from './components/Menu'
+import HomePage from './components/HomePage'
+import DriftPage from './components/DriftPage'
+import ForzaPage from './components/ForzaPage'
+import TimeAttackPage from './components/TimeAttackPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+	return (
+		<Router>
+		  <div>
+		    <Menu />
+		    <div className="page">
+		      <Route path="/" exact component={HomePage} />
+		      <Route path="/timeattack" component={TimeAttackPage} />
+					<Route path="/drift" component={DriftPage} />
+		      <Route path="/forza" component={ForzaPage} />
+		    </div>
+		  </div>
+		</Router>
+	);
 }
-
-export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Menu() {
+    return (
+        <nav className="menu">
+            <NavLink className="menu__item" activeClassName="menu__item-active" exact to="/">Главная</NavLink>
+            <NavLink className="menu__item" activeClassName="menu__item-active" to="/drift">Дрифт-такси</NavLink>
+            <NavLink className="menu__item" activeClassName="menu__item-active" to="/timeattack">Time Attack</NavLink>
+            <NavLink className="menu__item" activeClassName="menu__item-active" to="/forza">Forza Karting</NavLink>
+        </nav>
+    )
+}
